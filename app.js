2dabcc0856a66b4aa0e7173007fd47ed01e64472
hernan87ok <hernancordary@gmail.com>
2021-09-23 21:38:40 -0300

    Se agregan desafios hasta el numero 7

--- app.js
+++ app.js
@@ -17,3 +17,17 @@ app.get('/home', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.post('/register', (req, res) => {
+    res.redirect('/');
+});
+
+app.post('/', (req, res) => {
+    res.redirect('/');
+});
