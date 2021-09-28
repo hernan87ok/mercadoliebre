f923d0fb29e9bc2d5ec9255caaac470ccd41a34e
hernan87ok <hernancordary@gmail.com>
2021-09-28 19:53:02 -0300

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const app = express();
 
-app.listen(3030, () => {
+app.listen( process.env.PORT || 3030, () => {
 
     console.log('Servidor corriendo OK');
 }
